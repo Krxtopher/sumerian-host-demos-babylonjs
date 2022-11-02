46d15301f6963d9a8805eeac6ee712158c90ba69
Kris Schultz <krisschultz@gmail.com>
2022-11-02 10:12:01 -0700

    Add missing "demo-credentials.js" file.

---
+++ src/demo-credentials.js
@@ -0,0 +1,7 @@
+// TODO: Update the value below to match a Cognito Identity Pool created in your
+// AWS account. The unauthenticated IAM role for the pool (usually ending in the
+// suffix "Unauth_Role") must have the following managed permissions policies
+// assigned to it:
+//   - AmazonPollyReadOnlyAccess
+//   - AmazonLexRunBotsOnly
+export const cognitoIdentityPoolId = 'xxregionxx:xxx-xxx-xxx-xxx-xxx';
